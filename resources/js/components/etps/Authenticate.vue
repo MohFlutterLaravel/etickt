8d184065252089ae64e8239e88ed12cfc2af1fc0
MohFlutterLaravel <accilasuperette@gmail.com>
2022-11-01 13:29:12 +0100

    change

---
+++ resources/js/components/etps/Authenticate.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="container">
+    <div class="overlay">
+        <div class="login">
+            <h3>Essayez gratuitement</h3>
+        </div>
+        <div class="abonner">
+            <h2 class="text-orange" style="font-weight:bold">E-TICKET</h2>
+            <div class="switch">
+                <span class="active">
+                    MENSUEL
+                </span>
+                <span>
+                    ANNUEL
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
